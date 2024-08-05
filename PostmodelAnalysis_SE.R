adcbe6040e9d27cec1c1df5b5bc05dc9e225de8a
Mukhtar Yahaya <yahayamukhtar.m@gmail.com>
2024-08-05 15:03:53 +0100

    for loop for comparison and summary

--- PostmodelAnalysis_SE.R
+++ PostmodelAnalysis_SE.R
@@ -7,8 +7,11 @@ library(ggcorrplot)
 
 
 
-tover<-USdata3
+
+
+tover<-Net_with_structure_main
 tover<-na.omit(tover)
+tover<-tover[1:1500,]
 Vis.mat<-tover[,c(1,7,13)]
 names(Vis.mat)<-c("Early","Mid","Late")
 
@@ -168,7 +171,7 @@ Turnv.plot<-ggplot(stat.data, aes(fill=Predictor, y=Percentage, x=Phase )) +
   labs(y="Disjoint contribution (%)")
   #facet_grid(Phase ~ .)
 Turnv.plot
-ggsave("Turnv_US101.jpeg", plot =Turnv.plot ,
+ggsave("Turnv_His.tiff", plot =Turnv.plot ,
        width = 7, height = 5, dpi = 600)
 
 
@@ -187,23 +190,38 @@ boxplot(tover[,c(1,7,13)],col = c("grey"), boxwex = 0.5, ylab="Bray-Curtis turno
 par(mar = c(3,4.5,2,1.5))
 
 
-boxplot(tover[,7:12],col = c("grey",rep("white",5)), boxwex = 0.5, ylab="Bray-Curtis turnover", 
-        main="Mid phase", names =c("Int.","FE","Enc.","Pl.","An.","Fl."),
-        ylim = c(0, 1),cex.lab=2.0,cex.axis=2.0,cex.main=2.0 )
-abline(v=c(1.5),lty=2)
-
-boxplot(tover[,1:6],col = c("grey",rep("white",5)), boxwex = 0.5, ylab="Bray-Curtis turnover", 
-        main="Early phase", names =c("Int.","FE","Enc.","Pl.","An.","Fl."),
-        ylim = c(0, 1),cex.lab=2.0,cex.axis=2.0,cex.main=2.0 )
-abline(v=c(1.5),lty=2)
-
-boxplot(tover[,13:18],col = c("grey",rep("white",5)), boxwex = 0.5, ylab="Bray-Curtis turnover", 
-        main="Late phase", names =c("Int.","FE","Enc.","Pl.","An.","Fl."),
-        ylim = c(0, 1),cex.lab=2.0,cex.axis=2.0,cex.main=2.0 )
-abline(v=c(1.5),lty=2)
+boxplot(H2.mat,col = c("grey","white","white"), boxwex = 0.5, ylab="Specialisation (H'2)", 
+        main="Specialisation", names =c("Early","Mid", "Late"),
+        ylim = c(0, 1),cex.lab=2.0,cex.axis=2.0,cex.main=2.0)
 
 
-dev.copy(jpeg,"ToverFull_US101.jpeg",width = 300, height = 300,units = "mm", res = 600)
+
+boxplot(mod.mat,col = c("grey"), boxwex = 0.5, ylab="Modularity (Q)", 
+        main="Modularity", names =c("Early","Mid", "Late"),
+        ylim = c(0, 1),cex.lab=2.0,cex.axis=2.0,cex.main=2.0)
+
+
+boxplot(nes.mat/100,col = c("grey"), boxwex = 0.5, ylab="Nestedness (WNODA)", 
+        main="Nestedness", names =c("Early","Mid", "Late"),
+        ylim = c(0, 1),cex.lab=2.0,cex.axis=2.0,cex.main=2.0)
+# 
+# boxplot(tover[,7:12],col = c("grey",rep("white",5)), boxwex = 0.5, ylab="Bray-Curtis turnover", 
+#         main="Mid phase", names =c("Int.","FE","Enc.","Pl.","An.","Fl."),
+#         ylim = c(0, 1),cex.lab=2.0,cex.axis=2.0,cex.main=2.0 )
+# abline(v=c(1.5),lty=2)
+# 
+# boxplot(tover[,1:6],col = c("grey",rep("white",5)), boxwex = 0.5, ylab="Bray-Curtis turnover", 
+#         main="Early phase", names =c("Int.","FE","Enc.","Pl.","An.","Fl."),
+#         ylim = c(0, 1),cex.lab=2.0,cex.axis=2.0,cex.main=2.0 )
+# abline(v=c(1.5),lty=2)
+# 
+# boxplot(tover[,13:18],col = c("grey",rep("white",5)), boxwex = 0.5, ylab="Bray-Curtis turnover", 
+#         main="Late phase", names =c("Int.","FE","Enc.","Pl.","An.","Fl."),
+#         ylim = c(0, 1),cex.lab=2.0,cex.axis=2.0,cex.main=2.0 )
+# abline(v=c(1.5),lty=2)
+
+
+dev.copy(jpeg,"Turn&struc.tiff",width = 300, height = 300,units = "mm", res = 600)
 dev.off()
 
 
@@ -292,15 +310,6 @@ grid.arrange(eff_plot.bb3,eff_plot.be3, eff_plot.bp3,
 ggsave("eff.3.jpeg", plot =eff.plot.3 ,
        width = 16, height = 9, dpi = 600)
 #--------------------------------------------------
-cor_dat<-tover[c(1,7,13,20,21,26:30,33,35:37,39,41,42)]
-corr <- round(cor(cor_dat), 2)
-p.mat <- cor_pmat(cor_dat)
-ggcorrplot(corr, hc.order = FALSE,
-           lab = TRUE,p.mat = p.mat, insig = "blank")->USggfull
-USggfull
-ggsave("USggfull1.jpeg", plot =USggfull ,
-       width = 16, height = 9, dpi = 600)
-
 
 customise<-c("Vis.bc1"="Int. Early","Vis.bc2"="Int. Mid","Vis.bc3"="Int. Late",
              "H2.1"="Specialisation (Early)",
@@ -320,24 +329,40 @@ customise<-c("Vis.bc1"="Int. Early","Vis.bc2"="Int. Mid","Vis.bc3"="Int. Late",
              "var_rA"="V. Animal amplitude","var_sA"="V. Animal BP",
              "var_cP"="V. Plant density dep.","var_sigmaP"="V. Plant conversion eff.",
              "var_sigmaA"="V. Animal conversion eff.","var_a"="V. floral resource","var_sP"="V. Plant BP",
-             "var_uP"="V. Plant SL","var_uA"="V. Animal SL")
+             "var_uP"="V. Plant SL","var_uA"="V. Animal SL",
+              "bet.bc1"="FE. (Early)", "Enc.bc1"="Enc. (Early)", "XP.bc1"="Pl. (Early)",
+             "XA.bc1"="An. (Early)", "Fi.bc1"="Fl. (Early)", 
+             "bet.bc2"="FE. (Mid)", "Enc.bc2"="Enc. (Mid)", "XP.bc2"="Pl. (Mid)",
+             "XA.bc2"="An. (Mid)", "Fi.bc2"="Fl. (Mid)", 
+             "bet.bc3"="FE. (Late)", "Enc.bc3"="Enc. (Late)", "XP.bc3"="Pl. (Late)"
+             , "XA.bc3"="An. (Late)", "Fi.bc3"="Fl. (Late)")
+seasonParm<-c("mean_rP", "mean_rA", "mean_sP", "mean_sA", "mean_uP", 
+              "mean_uA", "var_rP", "var_rA", "var_sP", "var_sA", "var_uP", 
+              "var_uA")
 
 
-cor_dat<-tover[c(1,7,13,20,21,26:30,33,35:37,39,41,42)]
+
+cor_dat<-tover[c("Vis.bc1","Vis.bc2","Vis.bc3",seasonParm)]
 corr <- round(cor(cor_dat), 2)
 p.mat <- cor_pmat(cor_dat)
 ggcorrplot(corr[,1:3], hc.order = FALSE,
-           lab = TRUE)+
+           lab = TRUE,p.mat = p.mat[,1:3], insig = "blank")+
   scale_y_discrete(labels = customise)+
-  scale_x_discrete(labels = customise)->USggsigplot
+  scale_x_discrete(labels = customise)+
+  theme(text = element_text(size=18),
+        axis.text.x = element_text(size = 20))->USggfull
+USggfull
+ggsave("Int&parmgg.tiff", plot =USggfull ,
+       width = 14, height = 9, dpi = 600)
 
 
-ggsave("USggsig1.jpeg", plot =USggsigplot ,
-       width = 12, height = 6, dpi = 600)
 
 
 
-visdat::vis_cor(tover[,c(names(tover)[19:27],seasonParm)]) + 
+
+
+
+visdat::vis_cor(tover[,c(names(tover)[1:27])]) + 
   geom_text(aes(label = round(value,2)), 
                                              color = "black", size = 3)+
   scale_y_discrete(labels = customise)+
@@ -350,15 +375,16 @@ USggsigplot
 
 #### Network structures
 #c(names(tover)[19:27],seasonParm)
-cor_dat<-tover[,c(names(tover)[19:27],seasonParm)]
+cor_dat<-tover[,c(names(tover)[1:27])]
 corr <- round(cor(cor_dat), 2)
 p.mat <- cor_pmat(cor_dat)
-ggcorrplot((corr[,1:9]), hc.order = FALSE,
-           lab = TRUE, p.mat = (p.mat[,1:9]),insig = c("blank"))+
+ggcorrplot((corr[,19:27]), hc.order = FALSE,
+           lab = TRUE, p.mat = (p.mat[,19:27]),insig = c("blank"))+
   scale_y_discrete(labels = customise)+
-  scale_x_discrete(labels = customise)->ggcorplot
+  scale_x_discrete(labels = customise)+
+  theme(axis.text.x = element_text(size = 20))->ggcorplot
 
-ggsave("ggcor_Net.jpeg", plot =ggcorplot,
+ggsave("ggcor_Net&Turn.tiff", plot =ggcorplot,
        width = 16, height = 9, dpi = 600)
 
 visdat::vis_cor(tover[c(1:27)]) + geom_text(aes(label = round(value,2)), 
@@ -376,105 +402,67 @@ TukeyHSD(one.test.V)
 # mod test
 mod.mat<-data.frame(tover$mod.1,tover$mod.2,tover$mod.3)
 names(mod.mat)<-c("Early","Mid","Late")
-mod.mat<-melt(mod.mat,value.name = "Turnover",variable.name = "Period")
-one.test.V<-aov(Turnover ~ Period, data = mod.mat)
+one.test.V<-aov(Turnover ~ Period, data = melt(mod.mat,value.name = "Turnover",
+                                               variable.name = "Period"))
 summary(one.test.V)
 TukeyHSD(one.test.V)
 
 # nes test
 nes.mat<-data.frame(tover$nes.1,tover$nes.2,tover$nes.3)
 names(nes.mat)<-c("Early","Mid","Late")
-nes.mat<-melt(nes.mat,value.name = "Turnover",variable.name = "Period")
-one.test.V<-aov(Turnover ~ Period, data = nes.mat)
+one.test.V<-aov(Turnover ~ Period, data = melt(nes.mat,value.name = "Turnover",
+                                               variable.name = "Period"))
 summary(one.test.V)
 TukeyHSD(one.test.V)
 
 
 
-boxplot(H2.mat,col = c("grey"), boxwex = 0.5, ylab="Specialisation (H'2)", 
-        main="Specialisation", names =c("Early","Mid", "Late"),
-        ylim = c(0, 1),cex.lab=2.0,cex.axis=2.0,cex.main=2.0)
+# boxplot(H2.mat,col = c("grey"), boxwex = 0.5, ylab="Specialisation (H'2)", 
+#         main="Specialisation", names =c("Early","Mid", "Late"),
+#         ylim = c(0, 1),cex.lab=2.0,cex.axis=2.0,cex.main=2.0)
+# 
+# 
+# 
+# boxplot(mod.mat,col = c("grey"), boxwex = 0.5, ylab="Modularity (Q)", 
+#         main="Modularity", names =c("Early","Mid", "Late"),
+#         ylim = c(0, 1),cex.lab=2.0,cex.axis=2.0,cex.main=2.0)
+# 
+# 
+# boxplot(nes.mat/100,col = c("grey"), boxwex = 0.5, ylab="Nestedness (WNODA)", 
+#         main="Nestedness", names =c("Early","Mid", "Late"),
+#         ylim = c(0, 1),cex.lab=2.0,cex.axis=2.0,cex.main=2.0)
 
+# 
+# dev.copy(jpeg,"box_Net.jpeg",width = 300, height = 300,units = "mm", res = 600)
+# dev.off()
 
-
-boxplot(mod.mat,col = c("grey"), boxwex = 0.5, ylab="Modularity (Q)", 
-        main="Modularity", names =c("Early","Mid", "Late"),
-        ylim = c(0, 1),cex.lab=2.0,cex.axis=2.0,cex.main=2.0)
-
-
-boxplot(nes.mat/100,col = c("grey"), boxwex = 0.5, ylab="Nestedness (WNODA)", 
-        main="Nestedness", names =c("Early","Mid", "Late"),
-        ylim = c(0, 1),cex.lab=2.0,cex.axis=2.0,cex.main=2.0)
-
-
-dev.copy(jpeg,"box_Net.jpeg",width = 300, height = 300,units = "mm", res = 600)
-dev.off()
-
-seasonParm<-c("mean_rP", "mean_rA", "mean_sP", "mean_sA", "mean_uP", 
-  "mean_uA", "var_rP", "var_rA", "var_sP", "var_sA", "var_uP", 
-  "var_uA")
 
 
 
 # Model comparison
-H2.glm2 <-glm(formula = H2.2 ~ mean_rP + mean_rA + mean_sP + mean_sA + 
-                mean_uP + mean_uA + var_rP + var_rA + var_sP + var_sA + var_uP + var_uA, 
-                            data = tover,na.action = na.pass)
+dep<-c("H2.1", "mod.1", "nes.1", "H2.2", "mod.2", "nes.2", "H2.3", 
+       "mod.3", "nes.3")
 
-
-sum.H2.2<-summary.glm(H2.glm2)
-sum.H2.2
-
-
-with(summary(H2.glm2), 1 - deviance/null.deviance)
-vif(H2.glm1)->my_vif1
-my_vif1
-my_vif1[my_vif1>5]
-
-H2.comparison1 <- dredge(H2.glm1)
-head(H2.comparison1)
-as.matrix(mega.model.comparison1)->mmc1
-write.csv(mmc1,"mmc1.csv",row.names = F) 
-
-
-
-
-mod.glm1 <-glm(formula = mod.1 ~ mean_rP + mean_rA + mean_sP + mean_sA + 
-                 mean_uP + mean_uA + var_rP + var_rA + var_sP + var_sA + var_uP + var_uA, 
-              data = tover, na.action = na.pass)
-
-
-
-sum.mod.1<-summary.glm(mod.glm1)
-sum.mod.1
-
-
-with(summary(mod.glm1), 1 - deviance/null.deviance)
-vif(mod.glm1)->my_vif1
-my_vif1
-my_vif1[my_vif1>5]
-
-
-mod.comparison1 <- dredge(mod.glm1)
-head(mod.comparison1)
+comparison.table<-c()
+for(n in dep){
+  glm.com <-glm(formula = reformulate(seasonParm,n), 
+                data = tover,na.action = na.pass)
+  
+  comparison <- dredge(glm.com)
+  comparison[1,2:13]->comparison
+  rownames(comparison)<-n
+  comparison.table<-rbind(comparison.table,comparison)
+}
+# write.csv(comparison.table,"com_table.csv",row.names = T)
+for(i in 1:nrow(comparison.table)){
+  structure.glm<-glm(formula = reformulate(names(comparison.table[i,!is.na(comparison.table[i,])]),
+                                           rownames(comparison.table[i,])),
+                     data = tover, na.action = na.pass)
+  print(rownames(comparison.table[i,]))
+  print(summary.glm(structure.glm))
+  print(with(summary(structure.glm), 1 - deviance/null.deviance))
+  print("----------------------------------------------------")
+}
 
 
 
-
-nes.glm2 <-glm(formula = nes.2 ~ mean_rA + mean_sP + mean_sA 
-               + mean_uA + var_rP  + var_sP + var_sA + var_uA, 
-               data = tover, na.action = na.pass)
-
-
-sum.nes.2<-summary.glm(nes.glm2)
-sum.nes.2
-
-
-with(summary(nes.glm2), 1 - deviance/null.deviance)
-vif(nes.glm2)->my_vif1
-my_vif1
-my_vif1[my_vif1>5]
-
-
-nes.comparison2 <- dredge(nes.glm2)
-head(nes.comparison2)
