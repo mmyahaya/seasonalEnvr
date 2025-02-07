2e6ebc23df5caf95f986c28c5071204366e4f9be
Mukhtar Muhammed Yahaya <126781903+mmyahaya@users.noreply.github.com>
2025-02-07 21:52:12 +0200

    add change notation

--- PostmodelAnalysis_SE.R
+++ PostmodelAnalysis_SE.R
@@ -223,7 +223,11 @@ stat.data<-comm.analysis %>%
   pivot_longer(c(Early,Mid,Late),names_to = "Phase", values_to = "Percentage")
 
 stat.data$Phase <- factor(stat.data$Phase, levels = c("Early", "Mid","Late"))
-stat.data$Predictor<- factor(stat.data$Predictor, levels = c("Foraging effort","Encounter rate","Plant density","Animal density","Floral resource"))
+stat.data$Predictor<- factor(stat.data$Predictor, levels = c("Foraging effort",
+                                                             "Encounter rate",
+                                                             "Plant density",
+                                                             "Animal density",
+                                                             "Floral resource"))
 stat.data<-na.omit(stat.data)
 stat.data$Percentage<-stat.data$Percentage*100
 Turnv.plot<-ggplot(stat.data, aes(fill=Predictor, y=Percentage, x=Phase )) +
@@ -234,156 +238,22 @@ Turnv.plot<-ggplot(stat.data, aes(fill=Predictor, y=Percentage, x=Phase )) +
         axis.title.y = element_text(size = 14),  # Adjust y-axis label size
         legend.text = element_text(size = 14),
         legend.title = element_text(size = 14))+
-  labs(y="Disjoint contribution (%)")
+  labs(y="Disjoint contribution (%)")+
+  scale_fill_discrete(
+    labels = c("Foraging effort"=bquote(Delta ~ "Foraging effort"), 
+               "Encounter rate"=bquote(Delta ~ "Encounter rate"),
+               "Plant density"=bquote(Delta ~ "Plant density"),
+               "Animal density"=bquote(Delta ~ "Animal density"))  # Custom math labels
+  )
   #facet_grid(Phase ~ .)
 Turnv.plot
-ggsave("Turnv_His.tiff", plot =Turnv.plot ,
+ggsave("Turnv_His2.tiff", plot =Turnv.plot ,
        width = 7, height = 5, dpi = 600)
 
 
-#####
-
-# H2 test
-H2.mat<-data.frame(tover$H2.1,tover$H2.2,tover$H2.3)
-names(H2.mat)<-c("Early","Mid","Late")
-
-one.test.V<-aov(Turnover ~ Period, data = melt(H2.mat,value.name = "Turnover",
-                                               variable.name = "Period"))
-summary(one.test.V)
-TukeyHSD(one.test.V)
-
-# mod test
-mod.mat<-data.frame(tover$mod.1,tover$mod.2,tover$mod.3)
-names(mod.mat)<-c("Early","Mid","Late")
-one.test.V<-aov(Turnover ~ Period, data = melt(mod.mat,value.name = "Turnover",
-                                               variable.name = "Period"))
-summary(one.test.V)
-TukeyHSD(one.test.V)
-
-# nes test
-nes.mat<-data.frame(tover$nes.1,tover$nes.2,tover$nes.3)
-names(nes.mat)<-c("Early","Mid","Late")
-one.test.V<-aov(Turnover ~ Period, data = melt(nes.mat,value.name = "Turnover",
-                                               variable.name = "Period"))
-summary(one.test.V)
-TukeyHSD(one.test.V)
-
-#####
-#Plots
-layout(matrix(1:4, ncol = 2), widths = 1, heights = c(1,1), respect = FALSE)
-par(mar = c(3,4.5,2,1.5))
-boxplot(tover[,c("Vis.bc1","Vis.bc2","Vis.bc3")],col = c("grey"), boxwex = 0.5, ylab="Bray-Curtis turnover",
-        main="Interaction turnover", names =c("Early","Mid", "Late"),
-        ylim = c(0, 1),cex.lab=2.0,cex.axis=2.0,cex.main=2.0)
-
-
-par(mar = c(3,4.5,2,1.5))
-
-
-boxplot(H2.mat,col = c("grey","white","white"), boxwex = 0.5, ylab="Specialisation (H'2)",
-        main="Specialisation", names =c("Early","Mid", "Late"),
-        ylim = c(0, 1),cex.lab=2.0,cex.axis=2.0,cex.main=2.0)
 
 
 
-boxplot(mod.mat,col = c("grey"), boxwex = 0.5, ylab="Modularity (Q)",
-        main="Modularity", names =c("Early","Mid", "Late"),
-        ylim = c(0, 1),cex.lab=2.0,cex.axis=2.0,cex.main=2.0)
-
-
-boxplot(nes.mat/100,col = c("grey"), boxwex = 0.5, ylab="Nestedness (WNODA)",
-        main="Nestedness", names =c("Early","Mid", "Late"),
-        ylim = c(0, 1),cex.lab=2.0,cex.axis=2.0,cex.main=2.0)
-
-
-dev.copy(jpeg,"Turn&struc.tiff",width = 300, height = 300,units = "mm", res = 600)
-dev.off()
-
-
-# mtext()
-
-library(jtools)
-
-effect_plot(fitted_bc1,pred = bet.bc1,interval = TRUE ,plot.points = TRUE,
-            jitter = 0.05) +  labs(y = "Interaction", x= "Foraging effort")+
-  theme(text = element_text(size=18),axis.text.x = element_blank(),
-        axis.title.y = element_text(size = 20))->eff_plot.bb1
-effect_plot(fitted_bc1,pred = Enc.bc1,interval = TRUE ,plot.points = TRUE,
-            jitter = 0.05) +  labs(y = NULL, x= "Encounter rate")+
-  theme(text = element_text(size=18),axis.text.x = element_blank(),
-        axis.title.y = element_text(size = 20))->eff_plot.be1
-effect_plot(fitted_bc1,pred = XP.bc1,interval = TRUE ,plot.points = TRUE,
-            jitter = 0.05) +  labs(y = NULL, x= "Plant density")+
-  theme(text = element_text(size=18),axis.text.x = element_blank(),
-        axis.title.y = element_text(size = 20))->eff_plot.bp1
-
-
-
-
-effect_plot(fitted_bc2,pred = bet.bc2,interval = TRUE ,plot.points = TRUE,
-            jitter = 0.05) +  labs(y = "Interaction", x= "Foraging effort")+
-  theme(text = element_text(size=18),axis.text.x = element_blank(),
-        axis.title.y = element_text(size = 20))->eff_plot.bb2
-effect_plot(fitted_bc2,pred = Enc.bc2,interval = TRUE ,plot.points = TRUE,
-            jitter = 0.05) +  labs(y = NULL, x= "Encounter rate")+
-  theme(text = element_text(size=18),axis.text.x = element_blank(),
-        axis.title.y = element_text(size = 20))->eff_plot.be2
-effect_plot(fitted_bc2,pred = XP.bc2,interval = TRUE ,plot.points = TRUE,
-            jitter = 0.05) +  labs(y = NULL, x= "Plant density")+
-  theme(text = element_text(size=18),axis.text.x = element_blank(),
-        axis.title.y = element_text(size = 20))->eff_plot.bp2
-effect_plot(fitted_bc2,pred = XA.bc2,interval = TRUE ,plot.points = TRUE,
-            jitter = 0.05) +  labs(y = NULL, x= "Animal density")+
-  theme(text = element_text(size=18),axis.text.x = element_blank(),
-        axis.title.y = element_text(size = 20))->eff_plot.ba2
-
-
-effect_plot(fitted_bc3,pred = bet.bc3,interval = TRUE ,plot.points = TRUE,
-            jitter = 0.05) +  labs(y = "Interaction", x= "Foraging effort")+
-  theme(text = element_text(size=18),axis.text.x = element_blank(),
-        axis.title.y = element_text(size = 20))->eff_plot.bb3
-effect_plot(fitted_bc3,pred = Enc.bc3,interval = TRUE ,plot.points = TRUE,
-            jitter = 0.05) +  labs(y = NULL, x= "Encounter rate")+
-  theme(text = element_text(size=18),axis.text.x = element_blank(),
-        axis.title.y = element_text(size = 20))->eff_plot.be3
-effect_plot(fitted_bc3,pred = XP.bc3,interval = TRUE ,plot.points = TRUE,
-            jitter = 0.05) +  labs(y = NULL, x= "Plant density")+
-  theme(text = element_text(size=18),axis.text.x = element_blank(),
-        axis.title.y = element_text(size = 20))->eff_plot.bp3
-
-
-
-
-library(gridExtra)
-grid.arrange(eff_plot.bb1,eff_plot.be1, eff_plot.bp1,
-             eff_plot.bb2,eff_plot.be2, eff_plot.bp2,
-             eff_plot.bb3,eff_plot.be2,ncol = 3)->eff.plot
-
-
-grid.arrange(eff_plot.bb1,eff_plot.be1, eff_plot.bp1,
-             ncol = 3)->eff.plot.1
-
-
-ggsave("eff.1.jpeg", plot =eff.plot.1 ,
-       width = 16, height = 9, dpi = 600)
-
-
-
-grid.arrange(eff_plot.bb2,eff_plot.be2, eff_plot.bp2,eff_plot.ba2,
-             ncol = 4)->eff.plot.2
-
-
-ggsave("eff.2.jpeg", plot =eff.plot.2 ,
-       width = 16, height = 9, dpi = 600)
-
-
-
-grid.arrange(eff_plot.bb3,eff_plot.be3, eff_plot.bp3,
-             ncol = 3)->eff.plot.3
-
-
-ggsave("eff.3.jpeg", plot =eff.plot.3 ,
-       width = 16, height = 9, dpi = 600)
 #--------------------------------------------------
 
 customise<-c("Vis.bc1"="Int. Early",
@@ -424,105 +294,12 @@ customise<-c("Vis.bc1"="Int. Early",
              "XA.bc2"="An. (Mid)", "Fi.bc2"="Fl. (Mid)",
              "bet.bc3"="FE. (Late)", "Enc.bc3"="Enc. (Late)", "XP.bc3"="Pl. (Late)"
              , "XA.bc3"="An. (Late)", "Fi.bc3"="Fl. (Late)")
+
+
 seasonParm<-c("mean_rP", "mean_rA", "mean_sP", "mean_sA", "mean_uP",
               "mean_uA", "var_rP", "var_rA", "var_sP", "var_sA", "var_uP",
               "var_uA")
 
-
-
-cor_dat<-tover[c("Vis.bc1","Vis.bc2","Vis.bc3",seasonParm)]
-corr <- round(cor(cor_dat), 2)
-p.mat <- cor_pmat(cor_dat)
-corr<-corr[-c(4,5,7,10,11,13,14),1:3] %>%
-  as.data.frame() %>%
-  select(Vis.bc3,Vis.bc2,Vis.bc1) %>%
-  as.matrix()
-
-p.mat<-p.mat[-c(4,5,7,10,11,13,14),1:3] %>%
-  as.data.frame() %>%
-  select(Vis.bc3,Vis.bc2,Vis.bc1) %>%
-  as.matrix()
-
-
-
-ggcorrplot(corr,
-           hc.order = FALSE,lab_size = 8, tl.cex =18,
-           lab = TRUE,sig.level = 0.001,p.mat = p.mat, insig = "blank")+
-  scale_y_discrete(labels = customise)+
-  scale_x_discrete(labels = customise)->USggfull
-USggfull
- ggsave("Int&parm.tiff", plot =USggfull ,
-       width = 12, height = 7, dpi = 600)
-
-
-
-dput(rownames(corr))
-c( "mean_rP", "mean_sP", "mean_uP", "mean_uA", "var_sP")
-
-
-
-visdat::vis_cor(tover[,c(names(tover)[1:27])]) +
-  geom_text(aes(label = round(value,2)),
-                                             color = "black", size = 3)+
-  scale_y_discrete(labels = customise)+
-  scale_x_discrete(labels = customise,position = "top")
-
-
-pairs(cor_dat)
-
-USggsigplot
-
-#### Network structures
-#c(names(tover)[19:27],seasonParm)
-
-c.names<-c("Vis.bc1", "bet.bc1", "Enc.bc1", "XP.bc1", "XA.bc1", "Fi.bc1",
-           "H2.c1", "mod.c1", "nes.c1", "Vis.bc2", "bet.bc2", "Enc.bc2",
-           "XP.bc2", "XA.bc2", "Fi.bc2", "H2.c2", "mod.c2", "nes.c2", "Vis.bc3",
-           "bet.bc3", "Enc.bc3", "XP.bc3", "XA.bc3", "Fi.bc3", "H2.c3",
-           "mod.c3", "nes.c3")
-
-
-struct.names<-c("H2.c1", "mod.c1", "nes.c1", "H2.c2", "mod.c2", "nes.c2","H2.c3",
-                "mod.c3", "nes.c3")
-cor_dat<-tover[,c.names[19:27]]
-corr <- round(cor(cor_dat), 2)
-p.mat <- cor_pmat(cor_dat)
-ggcorrplot((corr[!(rownames(corr) %in% struct.names[7:9]),struct.names[7:9]]), hc.order = FALSE,
-           lab = TRUE, sig.level = 0.001,pch = 8,tl.cex = 18,
-           p.mat = (p.mat[!(rownames(corr) %in% struct.names[7:9]),struct.names[7:9]]),insig = c("blank"))+
-  scale_y_discrete(labels = customise)+
-  scale_x_discrete(labels = customise)->ggcorplot
-ggcorplot
-ggsave("ggcor_Net&Turn.tiff", plot =ggcorplot,
-       width = 16, height = 9, dpi = 600)
-
-visdat::vis_cor(tover[c(1:27)]) + geom_text(aes(label = round(value,2)),
-                                             color = "black", size = 3)
-
-# H2 test
-H2.mat<-data.frame(tover$H2.1,tover$H2.2,tover$H2.3)
-names(H2.mat)<-c("Early","Mid","Late")
-
-one.test.V<-aov(Turnover ~ Period, data = melt(H2.mat,value.name = "Turnover",
-                                               variable.name = "Period"))
-summary(one.test.V)
-TukeyHSD(one.test.V)
-
-# mod test
-mod.mat<-data.frame(tover$mod.1,tover$mod.2,tover$mod.3)
-names(mod.mat)<-c("Early","Mid","Late")
-one.test.V<-aov(Turnover ~ Period, data = melt(mod.mat,value.name = "Turnover",
-                                               variable.name = "Period"))
-summary(one.test.V)
-TukeyHSD(one.test.V)
-
-# nes test
-nes.mat<-data.frame(tover$nes.1,tover$nes.2,tover$nes.3)
-names(nes.mat)<-c("Early","Mid","Late")
-one.test.V<-aov(Turnover ~ Period, data = melt(nes.mat,value.name = "Turnover",
-                                               variable.name = "Period"))
-summary(one.test.V)
-TukeyHSD(one.test.V)
 
 
 
@@ -589,11 +366,24 @@ dev.copy(jpeg,"box_Net.tiff",width = 300, height = 300,units = "mm", res = 600)
 dev.off()
 
 
+disjoint_season<-function(fit,res){
+  comm.analysis<-data.frame("Var"=numeric(),"Perc"=numeric())
+  pred<-variable.names(fit)[-1]
+  Total.glm<-with(summary(fit), 1 - deviance/null.deviance)
+  for(n in 1:length(pred)){
+    f<-reformulate(pred[-n],res)
+    var.name<-pred[n]
+    g.l.m<-glm(formula = f,
+                    data = tover)
+    perc<-(Total.glm-with(summary(g.l.m), 1 - deviance/null.deviance))/Total.glm
+    comm.analysis<-rbind(comm.analysis,data.frame("Var"=var.name,"Perc"=perc))
+  }
+  comm.analysis
+}
 
-
-# Model comparison
-dep<-c("H2.c1", "mod.c1", "nes.c1", "H2.c2", "mod.c2", "nes.c2", "H2.c3",
-       "mod.c3", "nes.c3")
+dep<-c("Vis.bc1", "H2.c1", "mod.c1", "nes.c1",
+       "Vis.bc2", "H2.c2", "mod.c2", "nes.c2", 
+       "Vis.bc3", "H2.c3", "mod.c3", "nes.c3")
 
 comparison.table<-c()
 for(n in dep){
@@ -610,55 +400,57 @@ for(i in 1:nrow(comparison.table)){
   structure.glm<-glm(formula = reformulate(names(comparison.table[i,!is.na(comparison.table[i,])]),
                                            rownames(comparison.table[i,])),
                      data = tover, na.action = na.pass)
-  print(rownames(comparison.table[i,]))
+  cli::cli_h2(cli::col_blue(rownames(comparison.table[i,])))
+  print(structure.glm,rownames(comparison.table[i,]))
+ 
   print(summary.glm(structure.glm))
-  print(with(summary(structure.glm), 1 - deviance/null.deviance))
-  print("----------------------------------------------------")
+  cat("R_squared:",cli::col_red(round(with(summary(structure.glm), 1 - deviance/null.deviance),4)),"\n\n\n")
+  
 }
 
-# Model comparison
-dep<-c("H2.c1", "mod.c1", "nes.c1", "H2.c2", "mod.c2", "nes.c2", "H2.c3",
-       "mod.c3", "nes.c3")
-pre<-c("bet.bc1", "Enc.bc1", "XP.bc1", "XA.bc1", "Fi.bc1",
-      "bet.bc2", "Enc.bc2","XP.bc2", "XA.bc2", "Fi.bc2",
-comparison.table<-c()
-dep.list<-list(dep[1:3],dep[4:6],dep[7:9])
-comparison.list<-list()
-for(i in 1:3){
-  for(n in dep.list[[i]]){
-    if(n %in% dep.list[[1]] ){
-      glm.com <-glm.cons(formula = reformulate(pre[1:5],n),
-                         data = tover,cons = 1,na.action = na.pass)
-    } else if(n %in% dep.list[[2]]){
-      glm.com <-glm.cons(formula = reformulate(pre[6:10],n),
-                         data = tover,cons = 1,na.action = na.pass)
-    } else {
-      glm.com <-glm.cons(formula = reformulate(pre[11:15],n),
-                         data = tover,cons = 1,na.action = na.pass)
-    }
-    comparison <- dredge(glm.com)
-    comparison <- comparison[1,2:6]
-    rownames(comparison)<-n
-    comparison.table<-rbind(comparison.table,comparison)
-  }
-
-}
-
-comparison.list
-
-
+dis.table<-data.frame()
 # write.csv(comparison.table,"com_table.csv",row.names = T)
 for(i in 1:nrow(comparison.table)){
-  structure.glm<-glm.cons(formula = reformulate(names(comparison.table[i,!is.na(comparison.table[i,])]),
-                                           rownames(comparison.table[i,])),cons = 1,
+  structure.glm<-glm(formula = reformulate(names(comparison.table[i,!is.na(comparison.table[i,])]),
+                                           rownames(comparison.table[i,])),
                      data = tover, na.action = na.pass)
-  print(rownames(comparison.table[i,]))
-  print(summary.glm(structure.glm))
-  print(with(summary(structure.glm), 1 - deviance/null.deviance))
-  print("----------------------------------------------------")
+  #cli::cli_h2(cli::col_blue(rownames(comparison.table[i,])))
+  #print(disjoint_season(structure.glm,rownames(comparison.table[i,])))
+  dis<-disjoint_season(structure.glm,rownames(comparison.table[i,]))
+  dis$response<-rownames(comparison.table[i,])
+  dis.table<-rbind(dis.table,dis)
+  
+
 }
 
+dis.table<-dis.table %>% 
+mutate(Phase = case_when(
+  str_sub(response,-1) == "1" ~ "Early",
+  str_sub(response,-1) == "2" ~ "Mid",
+  str_sub(response,-1) == "3" ~ "Late",
+  TRUE ~ ""
+))
 
+
+
+dis_plot_early<-ggplot(dis.table %>% filter(Phase=="Early")) +
+  geom_bar( aes(fill=Var, y=Perc*100, x=response ),
+            position="dodge", stat="identity")+ theme_classic()+
+  theme(axis.text.x = element_text(size = 14),  # Adjust x-axis text size
+        axis.text.y = element_text(size = 14),  # Adjust y-axis text size
+        axis.title.x = element_text(size = 14),  # Adjust x-axis label size
+        axis.title.y = element_text(size = 14),  # Adjust y-axis label size
+        legend.text = element_text(size = 14),
+        legend.title = element_text(size = 14))+
+  labs(y="Disjoint contribution (%)")+theme(legend.position = "none")+
+  scale_x_discrete(labels=c("Vis.bc1"=bquote(Delta ~ "V"), 
+                            "H2.c1"=bquote(Delta ~ "H'2"), 
+                            "mod.c1"=bquote(Delta ~ "Q"),
+                            "nes.c1"=bquote(Delta ~ "N")))
+dis_plot_early
+
+  library(gridExtra)
+  grid.arrange(dis_plot_early,dis_plot_mid,dis_plot_late,ncol = 1)->eff.plot
 
 
 
